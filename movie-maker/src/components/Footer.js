f8497b3a079580cec099355a18985615c5d7725b
Damitha Perera <dami.sanka@gmail.com>
2021-05-15 16:00:39 +0530

    footer

---
+++ movie-maker/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  render(
+    <div className="footer">
+      <p>Copyright &copy; Movie maker {new Date().getFullYear()}</p>
+    </div>
+  ) 
+}
+
+export default Footer;
